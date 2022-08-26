f828334706ee38a65af3e4be6224c948289d5989
hitzges <hitzgesdiego@yahoo.de>
2022-08-26 17:44:39 +0200

    compressed instead of copmressed

--- JobScript_for_Data.py
+++ JobScript_for_Data.py
@@ -113,7 +113,7 @@ Global_Variables.set_var_to_global({'list_jobs':list_jobs,
 # In[12]:
 
 
-from States_and_Policies_copmressed import *
+from States_and_Policies_compressed import *
 
 
 # In[13]:
